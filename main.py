606aa9149a4f3fb424f4d97cbbe9267ad39cea36
tarneaux <contact.tarneaux@gmail.com>
2021-07-24 19:50:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,12 @@
 
 import setup_wizard
 import os
+import subprocess
+import sys
 
+def install_dependencies():
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "pip", "-U"])
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "discord"])
 
 if not os.path.isfile("config.json"):
     setup_wizard.Wizard()
